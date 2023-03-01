1229b4010e451b4353d29a5f014ae00de7ce4c27
ViperTools <cboatyt@gmail.com>
2023-03-01 12:54:05 -0600

    Update updater plugin

--- updater.lua
+++ updater.lua
@@ -5,6 +5,7 @@ function Plugin.Command()
         'curl "https://github.com/Syntad/smake-plugins/archive/refs/heads/main.zip" -L -o plugins.zip',
         'tar -xf plugins.zip',
         'rm plugins.zip',
+        'rm -rf ' .. pluginsFolder .. '/smake',
         'mv ./smake-plugins-main ' .. pluginsFolder .. '/smake'
     )
 end
