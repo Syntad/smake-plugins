b3c343ba3f16b6eb82fd5bf92d7f947b9dd8c478
ViperTools <cboatyt@gmail.com>
2023-03-03 22:26:32 -0600

    Fix updater on Windows

--- updater.lua
+++ updater.lua
@@ -1,11 +1,11 @@
-local pluginsFolder = platform.is_windows and '%APPDATA%\\Syntad\\Smake\\plugins' or '$HOME/.smake/plugins'
+local pluginsFolder = platform.is_windows and os.getenv('APPDATA') .. '\\Syntad\\Smake\\plugins' or '$HOME/.smake/plugins'
 
 function Plugin.Command()
     run(
         'curl "https://github.com/Syntad/smake-plugins/archive/refs/heads/main.zip" -L -o plugins.zip',
         'tar -xf plugins.zip',
         'rm plugins.zip',
-        'rm -rf ' .. pluginsFolder .. '/smake',
-        'mv ./smake-plugins-main ' .. pluginsFolder .. '/smake'
+        'rm -rf "' .. pluginsFolder .. '/smake"',
+        'mv ./smake-plugins-main "' .. pluginsFolder .. '/smake"'
     )
 end
