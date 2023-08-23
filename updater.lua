411d3a15f433d2680caf54cffd762523927031f8
ViperTools <cboatyt@gmail.com>
2023-08-23 15:28:18 -0500

    Make plugin updater use unzip

--- updater.lua
+++ updater.lua
@@ -3,7 +3,7 @@ local pluginsFolder = platform.is_windows and os.getenv('APPDATA') .. '\\Syntad\
 function Plugin.Command()
     run(
         'curl "https://github.com/Syntad/smake-plugins/archive/refs/heads/main.zip" -L -o ./plugins.zip',
-        'tar -xf ./plugins.zip',
+        'unzip -q ./plugins.zip',
         'rm ./plugins.zip',
         'rm -rf "' .. pluginsFolder .. '/smake"',
         'mv ./smake-plugins-main "' .. pluginsFolder .. '/smake"'
