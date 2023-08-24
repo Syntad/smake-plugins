f3d42616f5b78268c8a0a12491f49a4572adf00c
ViperTools <cboatyt@gmail.com>
2023-08-24 00:50:49 -0500

    Update lua version

--- smake/dependencyInstallers/lua.lua
+++ smake/dependencyInstallers/lua.lua
@@ -2,7 +2,7 @@ function Plugin.Import()
     return 'lua',
     --- @type fun(installer: installer)
     function(installer)
-        local folder = installer:DownloadAndUntar('https://www.lua.org/ftp/lua-5.4.4.tar.gz')
+        local folder = installer:DownloadAndUntar('https://www.lua.org/ftp/lua-5.4.6.tar.gz')
         folder:RunIn('cd src && make' .. (platform.is_windows and ' mingw' or '') .. '> /dev/null > err.log')
 
         installer:MakeIncludeFolder()
