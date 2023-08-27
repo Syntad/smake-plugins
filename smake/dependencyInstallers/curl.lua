dbd4cba125720a6225330bdd6306597627925cf4
ViperTools <cboatyt@gmail.com>
2023-08-26 20:45:46 -0500

    Make curl installer install openssl

--- smake/dependencyInstallers/curl.lua
+++ smake/dependencyInstallers/curl.lua
@@ -3,6 +3,8 @@ local fs = import('smake/utils/fs')
 function Plugin.Import()
     --- @type fun(installer: installer)
     return function(installer)
+        InstallDependency('openssl')
+        
         local folder = installer:DownloadAndUntar('https://github.com/curl/curl/releases/download/curl-8_2_1/curl-8.2.1.tar.gz')
         folder:RunIn('perl configure --with-openssl=$(realpath ../dependencies/openssl) && make')
 
