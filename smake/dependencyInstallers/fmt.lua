f5eb243e48ca2f342e5b9d12e6743264220edae6
ViperTools <cboatyt@gmail.com>
2023-08-26 20:28:42 -0500

    Improve fmt installer

--- smake/dependencyInstallers/fmt.lua
+++ smake/dependencyInstallers/fmt.lua
@@ -4,7 +4,7 @@ function Plugin.Import()
         local folder = installer:DownloadAndUnzip('https://github.com/fmtlib/fmt/releases/download/10.1.0/fmt-10.1.0.zip')
         folder:RunIn('cmake -DCMAKE_POSITION_INDEPENDENT_CODE=TRUE . && make fmt')
         folder:MoveInclude()
-        folder:Move('libfmt.a', installer:MakeLibraryFolder() .. '/libfmt.a')
+        folder:MoveLibraryFile('libfmt.a')
         folder:Delete()
     end
 end
